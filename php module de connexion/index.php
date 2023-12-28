678067ef33273963d7e9888ec2d77bb61f981468
maxence-grasso <151512620+maxence-grasso@users.noreply.github.com>
2023-12-28 16:11:26 +0100

    Update index.php

--- php module de connexion/index.php
+++ php module de connexion/index.php
@@ -3,9 +3,9 @@
 <html lang="fr">
 
 <head>
-    <title>La Plateforme_ DWWM 2023</title>
+    <title>Module de connexion</title>
     <meta charset="UTF-8">
-    <meta name="description" content="Promotion DWWM 2023-2024 La Plateforme_">
+    <meta name="description" content="Module">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
